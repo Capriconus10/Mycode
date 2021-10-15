12477f50af94c6a281f90dd113c88f96f6e9f2af
Capriconous10 <siddhivetal99@gmail.com>
2021-10-15 15:03:26 +0530

    Mycode Commit

---
+++ src/mycode/Mycode.java
@@ -0,0 +1,22 @@
+
+package mycode;
+
+/**
+ *
+ * @author siddhi
+ */
+public class Mycode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 20;
+        int y = 18;
+          if (x > y) {
+      System.out.println("x is greater than y");
+    }
+    
+    }
+}
