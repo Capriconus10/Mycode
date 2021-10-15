8ab8414237a1605e8c83d95c9e415a44b4d6cc94
Capriconous10 <siddhivetal99@gmail.com>
2021-10-15 15:48:36 +0530

    Mycode Modified

--- src/mycode/Mycode.java
+++ src/mycode/Mycode.java
@@ -12,7 +12,7 @@ public class Mycode {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int x = 20;
+        int x = 45;
         int y = 18;
           if (x > y) {
       System.out.println("x is greater than y");
